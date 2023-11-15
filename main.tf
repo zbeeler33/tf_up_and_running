df63ce401489c3b56b1003d32c04378cbceb93aa
Zach <zacharybeeler@gmail.com>
2023-11-15 14:24:57 -0600

    add variables

--- main.tf
+++ main.tf
@@ -2,11 +2,46 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-0fb653ca2d3203ac1"
-  instance_type = "t2.micro"
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
 
+resource "aws_instance" "example" {
+  ami                    = "ami-0fb653ca2d3203ac1"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.xhtml
+                nohup busybox httpd -f -p $(var.server_port) &
+                EOF
+
+  user_data_replace_on_change = true
   tags = {
     Name = "terraform-example"
   }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress = [{
+    description      = "Example instance SG"
+    from_port        = var.server_port
+    to_port          = var.server_port
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    security_groups  = []
+    self             = false
+  }]
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
 }
